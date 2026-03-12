9f2144daa50d7d455b6e06ae46e81dd533280ede
Fabio Luna <fabioluna20@gmail.com>
2026-03-13 00:11:47 +0100

    improving front end project

--- streamlit.py
+++ streamlit.py
@@ -1,67 +1,82 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from excel_utils import prepare_data
 from parameters import *
+import plotly.express as px
 
-def prepare_data(path):
-    df = pd.read_excel(path)
-    df_parametros = pd.read_excel(path, sheet_name='Parâmetros')
-
-    df['Data'] = pd.to_datetime(df['Data'])
-
-    #Criacao das novas colunas
-    df['Categoria'] = df['Local'].map(
-        df_parametros.set_index('Local')['Categoria']
-    ).fillna('Extra')
-
-    df['Pagamento?'] = np.where(
-        (df['Local'] == "CERN") & (df['Valor'] > 3000),
-        "Sim",
-        "Não"
-    )
-    df['Mês Pagamento'] = np.where(
-        df['Data'].dt.day >= 25, 
-        (df['Data'] + pd.DateOffset(months=1)), 
-        df['Data']
-    )
-    df['Mês Pagamento'] = df['Mês Pagamento'].dt.strftime('%m/%y')
-
-
-    saldo = []
-    for i, row in df.iterrows():
-        if i == 0:
-            saldo_atual = 3486
-
-        elif row['Pagamento?'] == 'Sim':
-            saldo_atual = 3486
-
-        else:
-            saldo_atual = saldo[-1] - row['Valor']
-        
-        saldo.append(saldo_atual)
-
-    df['Saldo'] = saldo
-
-    df['Mês Pagamento Atual?'] = np.where(df['Mês Pagamento'] == MES_ATUAL, 'Sim', 'Não')
-
-    return df
 
 def main():
     st.title("Dashboard de Gastos")
 
-    st.button('Reset Filters')
+    df = prepare_data()
 
-    df = prepare_data(path=PATH)
+    btn_filter = st.sidebar.header('Filtros')
+    if btn_filter:
+        print(1)
+
+    st.sidebar.button('Resetar Filtros')
+
+    categoria = st.sidebar.multiselect(
+        "Categoria",
+        df["Categoria"].unique(),
+        default=df["Categoria"].unique()
+    )
+
+    mes_pagamento_atual = st.sidebar.multiselect(
+        "Mês Pagamento Atual?",
+        df["Mês Pagamento Atual?"].unique(),
+        default=df["Mês Pagamento Atual?"].unique()
+    )
+
+    df_filtrado = df[
+        df['Categoria'].isin(categoria) &
+        df["Mês Pagamento Atual?"].isin(mes_pagamento_atual)
+    ]
+
     st.write('Table')
-    st.dataframe(df)
+    st.dataframe(df_filtrado)
+    # ------------------- Gastos por dia -------------------
+    # df_gastos = df[(df['Pagamento?'] == 'Não') & (df['Local'] != 'Wise Save')]
+    
+    # gastos_por_dia = df_gastos[df_gastos['Mês Pagamento Atual?'] == "Sim"].groupby('Data')['Valor'].sum().reset_index()
+    # # gastos_por_dia.index = gastos_por_dia.index.strftime('%d/%m')
 
-    col1, col2 = st.columns(2)
+    # fig1 = px.bar(
+    #     gastos_por_dia, 
+    #     x="Data",
+    #     y="Valor",
+    #     text="Valor"
+    # )
+    # fig1.update_traces(
+    #     texttemplate='%{text:.2f} CHF',
+    #     textposition='outside', 
+    #     marker_color='darkred'
+    # )
 
-    with col1:
-        st.write('coluna 1')
+    # fig1.update_layout(
+    #     title="Gastos por Dia - Mês Atual",
+    #     xaxis_title="Data",
+    #     yaxis_title="CHF",
+    #     showlegend=False
+    # )
+    # st.plotly_chart(fig1, use_container_width=True)
 
-    with col2:
-        st.write('coluna 2')
+    # # ------------------- Gastos por Categoria -------------------
+    # gastos_categoria = df_gastos[df_gastos['Mês Pagamento Atual?'] == "Sim"].groupby('Categoria')['Valor'].sum().sort_values(ascending=True).reset_index()
+
+    # fig2 = px.bar(gastos_categoria, x='Categoria', y='Valor', text='Valor')
+    # fig2.update_traces(texttemplate='%{text:.2f} CHF', textposition='outside', marker_color='darkred')
+    # fig2.update_layout(
+    #     title="Gastos por Categoria - Mês Atual",
+    #     xaxis_title="Categoria",
+    #     yaxis_title="CHF",
+    #     showlegend=False
+    # )
+
+    # st.plotly_chart(fig2, use_container_width=True)
+
+
 
 
 if __name__ == "__main__":
