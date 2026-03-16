f6859a1664286c1bf2d81ea0250be0ca4698c6c5
Fabio Luna <fabioluna20@gmail.com>
2026-03-16 21:20:59 +0100

    including last year data

--- support_files/transfer_data.py
+++ support_files/transfer_data.py
@@ -2,13 +2,16 @@ import sqlite3
 import pandas as pd
 
 PATH = "/Users/fabioluna/OneDrive - CERN/Swiss Control.xlsx"
+PATH_OLD = "/Users/fabioluna/Downloads/ControleMensal_v2.xlsx"
+
 transfer = False
-transfer_param = True
+transfer_param = False
+transfer_old = True
 
 if transfer:
     df = pd.read_excel(PATH)
 
-    conn = sqlite3.connect('./db/finance_control.db')
+    conn = sqlite3.connect('../db/finance_control.db')
 
     df.to_sql('gastos', conn, if_exists='replace', index=False)
 
@@ -17,10 +20,24 @@ if transfer:
 if transfer_param:
     df = pd.read_excel(PATH, sheet_name='Parâmetros')
 
-    conn = sqlite3.connect('./db/local_param.db')
+    conn = sqlite3.connect('../db/local_param.db')
 
     df.to_sql('param', conn, if_exists='replace', index=False)
 
     conn.close()
 
-### Criar tabela de parametros
+if transfer_old:
+    df = pd.read_excel(PATH_OLD, sheet_name='Controle')
+    
+    conn = sqlite3.connect('../db/finance_control.db')
+
+    df.to_sql('gastos', conn, if_exists='append', index=False)
+
+    conn.close()
+
+
+# conn = sqlite3.connect('../db/old_data.db')
+
+# df = pd.read_sql_query("SELECT * from gastos", conn)
+
+# print(df)
